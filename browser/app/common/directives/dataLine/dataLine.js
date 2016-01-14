f4ab60a4f4d640fe77d89cd3f51881c4f2d98c24
Alexius Wronka <Alexius@Alexiuss-MacBook-Pro.local>
2016-01-14 14:12:13 -0500

    added color change function fires on both directives for somereason when button is clicked

--- browser/app/common/directives/dataLine/dataLine.js
+++ browser/app/common/directives/dataLine/dataLine.js
@@ -8,7 +8,7 @@ app.directive('dataline', function($timeout,$window, $interval, $rootScope, glas
 
 
 
-            var rMin = 4, // min radius size
+            var rMin = 10, // min radius size
                 rMax = 35, // max radius size
                 rProperty = "overallRating", // "r" property will always be sampleSize
                 xScale, // scaling x-axis, so values al fit on line and are relative
@@ -18,58 +18,24 @@ app.directive('dataline', function($timeout,$window, $interval, $rootScope, glas
             d3.json("sampleJSON/" + scope.lineId + ".json", (data) => render(data,scope.lineId, "salary"));
 
             function render(data, typeProp, xProp) {
-                if(typeProp === 'jobTitle'){
+                //bad form must fix
                 data = s;
-                data = data.map(function(obj){
-                    var avgDiv = 1
-                    var total =  obj.salary;
-                    var ovrRat = obj.overallRating;
-                    for(var i = 0; i<data.length; i++){
-                        if(obj.jobTitle === data[i].jobTitle){
-                            avgDiv = avgDiv+1;
-                            total = total + data[i].salary;
-                            ovrRat =  ovrRat + data[i].overallRating;
-                        }
-                    }
-                    return {
-                        jobTitle: obj.jobTitle,
-                        salary: Math.floor(total/avgDiv),
-                        overallRating: ovrRat/avgDiv
-                    }
-                })
-                data = data.sort(function(a,b){
-                        var textA = a.jobTitle.toUpperCase();
-                        var textB = b.jobTitle.toUpperCase();
-                        return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
-                })
-                var jobData = [];
-                    for(var i = 0; i < data.length-1; i++){
-                        if(data[i].jobTitle !== data[i+1].jobTitle){
-                            jobData.push(data[i])
-                        }
-                    }
-                jobData = jobData.sort((a,b)=>a.salary-b.salary)
-                data = jobData.slice(jobData.length-7,jobData.length)
-                }
-                else if(typeProp === "company"){
-                data = s;
-                data = data.sort(function(a,b){return a.salary - b.salary})
-                data = data.slice(data.length-7,data.length)
-                }
+                data = glassData.filterByProp(typeProp, data)
+                data = data.slice(data.length-7, data.length)
 
                 console.log(data)
                 var lineToAppendTo = d3.select("#" + scope.lineId);
                 var tip = d3.tip()
                     .attr('class', 'd3-tip')
                     .offset([-10, 0])
-                    .html((d) => "<span>" + d[typeProp] + "</span><br><span>$" + d.salary + "</span>");
+                    .html((d) => "<span>" + d[typeProp] + "</span><br><span>$" + d.salary + "</span><br><span> work/life: "+ d.overallRating+ "</span>");
                 lineToAppendTo.call(tip); // attach hover info to bubbles
 
                 xScale = d3.scale.linear().domain(d3.extent(data, function(d){
                     return d[xProp]}))
                     .range([300, 1100]);
 
-                rScale = d3.scale.linear().domain(d3.extent(data, (d) => d[rProperty]))
+                rScale = d3.scale.linear().domain([1,5])
                     .range([rMin, rMax]);
 
                 var circles = lineToAppendTo
@@ -81,7 +47,12 @@ app.directive('dataline', function($timeout,$window, $interval, $rootScope, glas
 
                 circles
                     .attr("class", "data-line-circles")
-                    .attr("fill", "grey")
+                    .attr("fill", function(d){
+                        if(d.overallRating <2.5){
+                            return 'red'
+                        }
+                        return 'grey'
+                    })
                     .attr("cy", (d) => 200)
                     .attr("cx", (d,i)=>{console.log(d[xProp], xScale(i))
                                         return i*100+300
@@ -97,9 +68,9 @@ app.directive('dataline', function($timeout,$window, $interval, $rootScope, glas
                     .on('click', (d) => glassData.filterData('industry', 'Internet'));
 
         let startData = 0;
-         scope.getNewData = function(s){
-                console.log(startData, s.length)
-                var newData = s.slice(startData,startData+6)
+         scope.getNewData = function(data, typeProp){
+                var newData = glassData.filterByProp(typeProp, data)
+                newData = newData.slice(startData,startData+6)
                 var circles = d3.selectAll('circle')
                 circles
                     .transition()
@@ -118,10 +89,21 @@ app.directive('dataline', function($timeout,$window, $interval, $rootScope, glas
                     .attr("r",  (d) => {
                             console.log(d[rProperty])
                         return rScale(d[rProperty])})
+                circles
+                    .transition()
+                    .delay('1000')
+                    .attr("fill", function(d){
+                        if(d.overallRating <2.5){
+                            console.log('hit red')
+                            return 'red'
+                        }
+                        return 'grey'
+                    })
 
                     if(startData >= s.length-1){
                         startData = 0;
                     }
+
                     startData = startData + 6;
 
             }
--- browser/app/common/services/glassData.js
+++ browser/app/common/services/glassData.js
@@ -17,6 +17,45 @@ app.service('glassData', function($http) {
         }
     };
 
+    this.filterByProp = function(typeProp,data){
+                if(typeProp === 'jobTitle'){
+                data = data.map(function(obj){
+                    var avgDiv = 1
+                    var total =  obj.salary;
+                    var ovrRat = obj.overallRating;
+                    for(var i = 0; i<data.length; i++){
+                        if(obj.jobTitle === data[i].jobTitle){
+                            avgDiv = avgDiv+1;
+                            total = total + data[i].salary;
+                            ovrRat =  ovrRat + data[i].overallRating;
+                        }
+                    }
+                    return {
+                        jobTitle: obj.jobTitle,
+                        salary: Math.floor(total/avgDiv),
+                        overallRating: Math.floor((ovrRat/avgDiv)*100)/100
+                    }
+                })
+                data = data.sort(function(a,b){
+                        var textA = a.jobTitle.toUpperCase();
+                        var textB = b.jobTitle.toUpperCase();
+                        return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+                })
+                var jobData = [];
+                    for(var i = 0; i < data.length-1; i++){
+                        if(data[i].jobTitle !== data[i+1].jobTitle){
+                            jobData.push(data[i])
+                        }
+                    }
+                jobData = jobData.sort((a,b)=>a.salary-b.salary)
+                data = jobData
+                }
+                else if(typeProp === "company"){
+                data = data.sort(function(a,b){return a.salary - b.salary})
+                }
+        return data
+    }
+
     this.filterData = function(prop, str) {
         console.log(prop, str)
 
