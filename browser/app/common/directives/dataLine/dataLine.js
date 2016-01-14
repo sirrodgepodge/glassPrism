cf68656c13bed2b128b190ec7d1f0121bd5becf2
Alexius Wronka <Alexius@Alexiuss-MacBook-Pro.local>
2016-01-14 13:45:49 -0500

    filter for software average

--- browser/app/common/directives/dataLine/dataLine.js
+++ browser/app/common/directives/dataLine/dataLine.js
@@ -18,15 +18,51 @@ app.directive('dataline', function($timeout,$window, $interval, $rootScope, glas
             d3.json("sampleJSON/" + scope.lineId + ".json", (data) => render(data,scope.lineId, "salary"));
 
             function render(data, typeProp, xProp) {
+                if(typeProp === 'jobTitle'){
+                data = s;
+                data = data.map(function(obj){
+                    var avgDiv = 1
+                    var total =  obj.salary;
+                    var ovrRat = obj.overallRating;
+                    for(var i = 0; i<data.length; i++){
+                        if(obj.jobTitle === data[i].jobTitle){
+                            avgDiv = avgDiv+1;
+                            total = total + data[i].salary;
+                            ovrRat =  ovrRat + data[i].overallRating;
+                        }
+                    }
+                    return {
+                        jobTitle: obj.jobTitle,
+                        salary: Math.floor(total/avgDiv),
+                        overallRating: ovrRat/avgDiv
+                    }
+                })
+                data = data.sort(function(a,b){
+                        var textA = a.jobTitle.toUpperCase();
+                        var textB = b.jobTitle.toUpperCase();
+                        return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+                })
+                var jobData = [];
+                    for(var i = 0; i < data.length-1; i++){
+                        if(data[i].jobTitle !== data[i+1].jobTitle){
+                            jobData.push(data[i])
+                        }
+                    }
+                jobData = jobData.sort((a,b)=>a.salary-b.salary)
+                data = jobData.slice(jobData.length-7,jobData.length)
+                }
+                else if(typeProp === "company"){
                 data = s;
                 data = data.sort(function(a,b){return a.salary - b.salary})
                 data = data.slice(data.length-7,data.length)
+                }
+
                 console.log(data)
                 var lineToAppendTo = d3.select("#" + scope.lineId);
                 var tip = d3.tip()
                     .attr('class', 'd3-tip')
                     .offset([-10, 0])
-                    .html((d) => "<span>" + d[typeProp] + "</span><br><span>$" + d.salary + "</span><br><span>rating: "+ d.overallRating + "</span><br><span>" + d.company + "</span>");
+                    .html((d) => "<span>" + d[typeProp] + "</span><br><span>$" + d.salary + "</span>");
                 lineToAppendTo.call(tip); // attach hover info to bubbles
 
                 xScale = d3.scale.linear().domain(d3.extent(data, function(d){
