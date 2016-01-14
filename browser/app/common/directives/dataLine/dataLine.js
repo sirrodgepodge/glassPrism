ebc28fb80ae1e0b713b051e27f5fc8353be1a27b
Alexius Wronka <Alexius@Alexiuss-MacBook-Pro.local>
2016-01-14 10:50:44 -0500

    added data transition function

--- browser/app/common/directives/dataLine/dataLine.js
+++ browser/app/common/directives/dataLine/dataLine.js
@@ -3,16 +3,8 @@ app.directive('dataline', function($timeout,$window, $interval, $rootScope, glas
 
     var lineCirclesLink = function(scope, element, attrs) {
                 glassData.filterData('industry', 'Internet').then(function(s){
-                
+            scope.allData = s;
             //remove later
-            scope.getNewData = function(){
-                var circles = d3.selectAll('circle')
-                circles
-                    .transition()
-                    .duration(500)
-                    .attr("r",  (d) => (rScale(d[rProperty]*2)))
-
-            }
 
 
 
@@ -28,13 +20,13 @@ app.directive('dataline', function($timeout,$window, $interval, $rootScope, glas
             function render(data, typeProp, xProp) {
                 data = s;
                 data = data.sort(function(a,b){return a.salary - b.salary})
-                data = data.slice(data.length-6,data.length)
+                data = data.slice(data.length-7,data.length)
                 console.log(data)
                 var lineToAppendTo = d3.select("#" + scope.lineId);
                 var tip = d3.tip()
                     .attr('class', 'd3-tip')
                     .offset([-10, 0])
-                    .html((d) => "<span>" + d[typeProp] + "</span><br><span>$" + d.salary + "</span><br><span>w/l "+ d.overallRating + "</span>");
+                    .html((d) => "<span>" + d[typeProp] + "</span><br><span>$" + d.salary + "</span><br><span>rating: "+ d.overallRating + "</span>");
                 lineToAppendTo.call(tip); // attach hover info to bubbles
 
                 xScale = d3.scale.linear().domain(d3.extent(data, function(d){
@@ -54,7 +46,9 @@ app.directive('dataline', function($timeout,$window, $interval, $rootScope, glas
                     .attr("class", "data-line-circles")
                     .attr("fill", "grey")
                     .attr("cy", (d) => 200)
-                    .attr("cx", (d) => xScale(d[xProp]))
+                    .attr("cx", (d,i)=>{console.log(d[xProp], xScale(i))
+                                        return i*100+300
+                                        })
                     .attr('r', 0)
                     .transition()
                     .delay((d,i)=>(i*50))
@@ -64,6 +58,34 @@ app.directive('dataline', function($timeout,$window, $interval, $rootScope, glas
                     .on('mouseover', tip.show)
                     .on('mouseout', tip.hide)
                     .on('click', (d) => glassData.filterData('industry', 'Internet'));
+
+
+         scope.getNewData = function(s){
+                var circles = d3.selectAll('circle')
+                circles
+                    .transition()
+                    .attr('r', 0)
+                    .remove()
+
+                circles
+                    .data(s)
+                    .enter()
+                    .append('circle')
+
+                circles
+                    .attr("class", "data-line-circles")
+                    .attr("fill", "grey")
+                    .attr("cy", (d) => 200)
+                    .attr("cx", (d,i)=>{console.log(d[xProp], xScale(i))
+                                        return i*100+300
+                                        })
+                    .attr('r', 0)
+                    .transition()
+                    .delay((d,i)=>(i*50))
+                    .attr("r",  (d) => rScale(d[rProperty]))
+
+            }
+
             }
         })
             
