9aae1f0fa95f3fd71a079de4224f7867c35eb2b7
awronka <alexiusmichael@gmail.com>
2016-01-26 09:52:14 -0500

    fix

--- browser/app.js
+++ browser/app.js
@@ -6,5 +6,5 @@ app.config(function($urlRouterProvider, $locationProvider) {
     // This turns off hashbang urls (/#about) and changes it to something normal (/about)
     $locationProvider.html5Mode(true);
     // If we go to a URL that ui-router doesn't have registered, go to the "/" url.
-    $urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise('');
 });
